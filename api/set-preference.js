b4d306a7d2a083cde83ae0890915350e476250ea
Sourav Kumar <97384250+sourav-kumar4297@users.noreply.github.com>
2026-02-16 13:21:36 +0530

    Update CORS origin for set-preference handler

--- api/set-preference.js
+++ api/set-preference.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   // CORS setup
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', 'https://essentiahome.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.essentiahome.com');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     return res.status(200).end();
