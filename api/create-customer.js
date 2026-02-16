eb02d82f30c1eb586f622f575cb57d6d443f048b
Sourav Kumar <97384250+sourav-kumar4297@users.noreply.github.com>
2026-02-16 13:22:38 +0530

    Update CORS origin to include www subdomain

--- api/create-customer.js
+++ api/create-customer.js
@@ -1,6 +1,6 @@
 // /api/create-customer.js
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://essentiahome.com");
+  res.setHeader("Access-Control-Allow-Origin", "https://www.essentiahome.com");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
